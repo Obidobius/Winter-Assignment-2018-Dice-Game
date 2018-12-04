bafe5e62efc54320da1aab1f94c6c9867691328a
Oda Gabriela Undertun Antonsen <oda.antonsen@yahoo.com>
2018-12-04 12:10:27 +0100

    4.12.2018
    4.12.2018

--- scripts/boardGame_updated.js
+++ scripts/boardGame_updated.js
@@ -1,164 +1,198 @@
-var player1Url = localStorage.getItem('Player 1');
-var player2Url = localStorage.getItem('Player 2');
+//-----------------INDEX-----------------//
+// 1. Variables
+// 2. Player Check
+// 3. Dice System
+// 4. Moving System
+//--4.1 Player 1
+//--4.2 Player 2
+// 5. Turn System
+// 6. Player Location Text
+// 7. Winner Redirect Page
 
-fetch(player1Url)
-    .then((response) => { return response.json() })
-    .then((result) => {
-        player1 = result.aliases[0]
-    })
-
-fetch(player2Url)
-    .then((response) => { return response.json() })
-    .then((result) => {
-        player2 = result.aliases[0]
-    })
+//Get Players icons from previous Page as portrait
+var player1Name = localStorage.getItem('Player 1');
+var player2Name = localStorage.getItem('Player 2');
 
 document.getElementById('leftPlayerIcon1').innerHTML += '<img class="[ selection__icon ]" src="icons/' + localStorage.getItem('Player_1_id') + '.png" />';
 document.getElementById('rightPlayerIcon2').innerHTML += '<img class="[ selection__icon ]" src="icons/' + localStorage.getItem('Player_2_id') + '.png" />';
-document.getElementById('leftPlayerIcon1').innerHTML += '<p class="card-title [ card-body__characterTitle ]">'+ localStorage.getItem('player 1') +'</p>'
-document.getElementById('rightPlayerIcon2').innerHTML += '<p class="card-title [ card-body__characterTitle ]">'+ localStorage.getItem('player 2') +'</p>'
 
-var infoText = document.getElementById("text_field");
-var inText = document.createElement("p");
-inText.setAttribute("id","battleText");
-infoText.appendChild(inText);
+                                //-----------------1. VARIABLES-----------------//
+var turn = 1;   //Players Turn
+var tile = 1;   //Player 1
+var tile2 = 1;  //Player 2
 
+                            //----------------2. Players are dead or not------------//
+pDeath1 = false;
+pDeath2 = false;
+                
+var playerTile = document.getElementById(tile);     //Player 1 Tiles
+var playerTile2 = document.getElementById(tile);    //Player 2 Tiles
 
-var infoText2 = document.getElementById("text_field");
-var inText2 = document.createElement("p");
-inText2.setAttribute("id","battleText2");
-infoText2.appendChild(inText2);
-
-
-                //----------------------------------Dice system-----------------------------//
-var turn = 1;
-var turn2 = 2;
-
-var tile = 1;
-
-var player1 = turn;
-var player2 = turn2;
-
-var playerTile = document.getElementById(tile);
-var playerTile2 = document.getElementById(tile);
-
+ //Get Player Icons from previous Page as moving pieces
 playerTile.innerHTML = '<img id="plr_1" class="[ selection__icon ]" src="icons/' + localStorage.getItem("Player_1_id") + '.png" />';
 playerTile2.innerHTML = '<img id="plr_2" class="[ selection__icon ]" src="icons/' + localStorage.getItem("Player_2_id") + '.png" />';
 
+                //----------------------------------3. Dice system-----------------------------//
 var roll = {
     corners: 6,
     throw: function () {
         var randomTrow = Math.floor(Math.random() * this.corners) + 1;
         return randomTrow;
     }
-}
+};
 
 function showPrint(result) {
     var dicePlacement = document.getElementById("dicePlacement");
-    console.log(result);
     dicePlacement.innerHTML = result;
 };
-
 var terning = document.getElementById("diceRoll");
 terning.onclick = function () {
     var result = roll.throw();
     moveIcons(result);
-    showTiles(tile);
+    showTiles();
     showPrint(result);
-
     winner(tile);
 };
 
+//------------------------------- 4. Moving current icon and Removin previous icon -------------------------//
 
-//-------------------------------Moving current icon and Removin previous icon -------------------------//
-
-                        //-----------------player 1-----------------//
-function movePiece(){
-    var previous_pos = document.getElementById("plr_1");
+//Trap tile Location
+var traps = [5, 13, 19, 25, 29]; 
+                                //-----------------4.1 Player 1-----------------//
+// Trap Triggers
+function movePiece() {
+    let previous_pos = document.getElementById("plr_1");
     if (previous_pos) {
         previous_pos.parentNode.removeChild(previous_pos);
     }
-    for(var i = 1; i< 31; i++){
-        if(tile === i){
-          var moves = document.getElementById(i); 
+    if (traps.indexOf(tile) !== -1) {
+        if (tile === 5) {
+            tile = tile - 2;
+            alert("A Dire Wolf Appears, you moved back 2 steps");
+        }
+        else if (tile === 13) {
+            tile = tile - 12;
+            alert("You got Arrested from mooning Cersei, you go back to start");
+        }
+        else if (tile === 19) {
+            tile = tile - 4;
+            alert("The bridge infront have collapsed, you walk around to find another way, you go back 4 Tiles");
+        }
+        else if (tile === 25) {
+            tile = tile - 4;
+            alert("A rockslide triggers above you, you run back 4 tiles");
+        }
+        else if (tile === 29) {
+            tile = tile - 28;
+            pDeath1 = true;
+            alert("A White walker appears, you die a terrible death and become one of them!");
+        }
+    };
+    for (let i = 1; i < 31; i++) {
+        if (tile === i) {
+            let moves = document.getElementById(i);
             moves.innerHTML += '<img id="plr_1" class="[ selection__icon ]" src="icons/' + localStorage.getItem("Player_1_id") + '.png" />';
             console.log(moves[i]);
         }
     }
 };
 
-                        //-----------------Player 2-----------------//
-function movePiece2(){
-    var previous_pos2 = document.getElementById("plr_2");
+                             //-----------------4.2 Player 2-----------------//
+// Trap Triggers     
+function movePiece2() {
+    let previous_pos2 = document.getElementById("plr_2");
     if (previous_pos2) {
         previous_pos2.parentNode.removeChild(previous_pos2);
     }
-    for(var i = 1; i< 31; i++){
-        if(tile === i){
-          var moves = document.getElementById(i); 
+    if (traps.indexOf(tile2) !== -1) {
+        if (tile2 === 5) {
+            tile2 = tile2 - 2;
+            alert("A Dire Wolf Appears, you moved back 2 steps");
+        }
+        else if (tile2 === 13) {
+            tile2 = tile2 - 12;
+            alert("You got Arrested from mooning Cersei, you go back to start");
+        }
+        else if (tile2 === 19) {
+            tile2 = tile2 - 4;
+            alert("The bridge infront have collapsed, you walk around to find another way, you go back 4 Tiles");
+        }
+        else if (tile2 === 25) {
+            tile2 = tile2 - 4;
+            alert("A rockslide triggers above you, you run back 4 tiles");
+        }
+        else if (tile2 === 29) {
+            tile2 = tile2 - 28;
+            pDeath2 = true;
+            alert("A White walker appears, you die a terrible death and become one of them!");
+        }
+    };
+    for (let j = 1; j < 31; j++) {
+        if (tile2 === j) {
+            let moves = document.getElementById(j);
             moves.innerHTML += '<img id="plr_2" class="[ selection__icon ]" src="icons/' + localStorage.getItem("Player_2_id") + '.png" />';
-            console.log(moves[i]);
+            console.log(moves[j]);
         }
     }
 };
 
-
-                         //-----------------and turns-----------------//
-
-
-                         
-//switch turns{
-//    case turn === 1:
-//      movePiece2()
-//      turn = 2;
-//  break;
-//  case turn = 2:
-//      movePiece()
-//  break;
-//}
+                         //-----------------5. TURNS----------------//
 
 function moveIcons(result) {
+    if (pDeath1 && pDeath2) {
+        alert("Wtf all players are dead");
+        window.location.href = "allDead.html";
+    }
     if (turn === 1) {
+        for (let i = 0; i < result; i++) {
+            tile = tile + 1;
+        }
+        movePiece()
+        if (!pDeath2) {
+            turn = 2
+            if (result === 6) {
+                console.log("trigger player 1 dice")
+                alert("player 1 hit 6, roll again!")
+                movePiece()
+                turn = 1
+            }
+        }
+    }
+    else if (turn === 2) {
+        for (let i = 0; i < result; i++) {
+            tile2 = tile2 + 1;
+        }
         movePiece2()
-        turn = 2;
-  
+        if (!pDeath1) {
+            turn = 1
+            if (result === 6) {
+                console.log("trigger player 2 dice")
+                alert("player 2 hit 6, roll again!")
+                movePiece2()
+                turn = 2
+            }
+        }
+    }
+};
+            //---------------6. PLAYER TILE LOCATION TEXT--------------//
+function showTiles() {
+    var inText = document.getElementById("battleText");
+
+    if (turn === 2) {
+        inText.innerHTML = "Player 1 moved to tile: " + tile;
     }
     else {
-        turn = 1;
-        movePiece()
-    
+        inText.innerHTML = "Player 2 moved to tile: " + tile2;
     }
-    for (var i = 0; i < result; i++) {
-        tile = tile + 1;
-    }
-    
- 
 
-    playerTile = document.getElementById(tile);
-    console.log(playerTile);
-
-   // playerTile2 = document.getElementById(tile):
-   // console.log(playerTile2);
 };
 
-
-//---------------PLAYER TEXT TILE LOCATION TEXT--------------//
-function showTiles(result){  
-    var inText = document.getElementById("battleText");
-    inText.innerHTML = "Player 1 moved to: " + result;
-};
-function showTiles2(result){
-    var inText2 = document.getElementById("battletext2");
-    inText2.innerHTML = "Player 2 moved to: " + result;
-}
-
-
-function winner(result){
-    if(tile > 29){
-        window.location.href="success_Page.html";
-
+                //-----------------7. Direct Winner to new Page----------------//
+function winner(result) {
+    if (tile > 29) {
+        window.location.href = "success_Page.html";
     }
-    else{
-
+    else if (tile2 > 29) {
+        window.location.href = "success_Page.html";
     }
 };
---
+++ scripts/success_page.js
@@ -0,0 +1,9 @@
+function draw() {
+    var ctx = document.getElementById('canvasPlace').getContext('2d');
+    ctx.font = '10px serif';
+    ctx.fillText('Congratulations of surviving the race!', 80, 30);
+  }draw();
+
+
+//ctx.width = canvas.clientWidth;
+//ctx.height = canvas.clientHeight;
