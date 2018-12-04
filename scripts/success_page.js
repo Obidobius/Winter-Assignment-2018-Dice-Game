409f3a576b72db8f41321a0426d6e9e514e9d97a
Oda Gabriela Undertun Antonsen <oda.antonsen@yahoo.com>
2018-12-04 14:37:19 +0100

    Update Canvas

--- scripts/success_page.js
+++ scripts/success_page.js
@@ -1,10 +1,18 @@
 function draw() {
-    var ctx = document.getElementById('canvasPlace').getContext('2d');
-    ctx.font = '13px serif';
-    ctx.fillText('Congratulations of surviving the race!', 70, 30);
-    this.ctx.heigh;
-  }draw();
+    var canvas = document.getElementById('canvasPlace')
+    var ctx = canvas.getContext('2d');
+    canvas.height = 600;
+    canvas.width = 800;
+    ctx.font = '70px Playfair Display SC, serif';
+    ctx.fill = "black";
+    ctx.fillText('Congratulations!', 60, 80);
+    ctx.font = '35px Playfair Display SC, serif';
+    ctx.fill = "black";
+    ctx.fillText('You survived!', 260,140)
+    
+  };
 
+draw();
+function animate(){
 
-//ctx.width = canvas.clientWidth;
-//ctx.height = canvas.clientHeight;
+}
