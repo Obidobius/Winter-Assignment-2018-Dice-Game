b708b2201ee8790212c653f97d1499ce05043d37
Oda Gabriela Undertun Antonsen <oda.antonsen@yahoo.com>
2018-12-06 14:44:05 +0100

    2 tasks remaining

--- scripts/success_page.js
+++ scripts/success_page.js
@@ -3,17 +3,19 @@ var winner2 = localStorage.getItem('player_02_id');
 var collect = document.getElementsByClassName("winner");
 
 
-function showWinner(winner){
-    if(winner1 === localStorage.getItem('player_01_id')){
-        let collect = document.getElementById("winner");
-        collect.innerHTML = '<img src="../icons/' + localStorage.getItem("Player_1_id") + '.png" />' + "Player One Won";
+function showWinner(){
+    var player1 =  localStorage.getItem('player_01_id');
+    let collect = document.getElementById("winner");    
+    console.log(player1)
+    if(player1 === null || player1 === undefined || player1 === "" ){
+        alert("Player 2 has won the game");
+        collect.innerHTML = '<img id="playerTwo" src="./icons/' + localStorage.getItem("Player_2_id") + '.png" />' + "<p class='win' >Player Two Won! </p>";
     }
     else{
-        let collect = document.getElementById("winner");
-        collect.innerHTML = '<img src="../icons/' + localStorage.getItem("Player_2_id") + '.png" />' + "Player Two Won";
+        alert("Player 1 has won the game");
+        collect.innerHTML = '<img id="playerOne" src="./icons/' + localStorage.getItem("Player_1_id") + '.png" />' + "<p class='win' >Player One Won! </p>";
     }
 };
-
 
 function draw() {
     var canvas = document.getElementById('canvasPlace')
@@ -22,10 +24,10 @@ function draw() {
     canvas.width = 800;
     ctx.font = '70px Playfair Display SC, serif';
     ctx.fill = "black";
-    ctx.fillText('Congratulations!', 60, 80);
-    ctx.font = '35px Playfair Display SC, serif';
+    ctx.fillText('Congratulations!', 90, 100);
+    ctx.font = '60px Playfair Display SC, serif';
     ctx.fill = "black";
-    ctx.fillText('You survived!', 260,140)
+    ctx.fillText('You survived!', 220,170)
     
   };
 
